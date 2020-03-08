bf7f33df93891bd00b0d1f072aa0e2ddd35cb339
Anish Abraham <vjanish1984@gmail.com>
2020-03-08 14:27:30 +0000

    Updated Email reports, Add things to do and more description around things done

--- Email_reports.py
+++ Email_reports.py
@@ -1,15 +1,18 @@
 import requests
 import json
 import datetime
+import time
 
-##Get the current date in format DD-MM-YYYY
+##Get the current date in format YYYY-MM-DD
 x = datetime.datetime.now()
-d = (x.strftime("%d")+"-"+x.strftime("%m")+"-"+x.strftime("%Y"))
+d = (x.strftime("%Y")+"-"+x.strftime("%m")+"-"+x.strftime("%d"))
 currentdate=str(d)
 
-##Get the start date in format DD-MM-YYYY, past 30 days
+##Get the start date in format YYYY-MM-DD, past 30 days
+## The number in datetime.timedelta(30) can be set to whatever the last days report required.
+
 s = datetime.datetime.now() - datetime.timedelta(30)
-sd = (s.strftime("%d")+"-"+s.strftime("%m")+"-"+s.strftime("%Y"))
+sd = (s.strftime("%Y")+"-"+s.strftime("%m")+"-"+s.strftime("%d"))
 startdate=str(sd)
 
 token = "77937dc9-000c-4d70-b3db-0af0096c27b6"
@@ -28,7 +31,7 @@ def runreports(type):
     rid = data['reportResult']['id']
     return rid
 
-#sid = runreports('appCost')
+#sid = runreports('instanceCost')
 #print(sid)
 
 # Fetch reports
@@ -36,10 +39,20 @@ def getreports(sid):
     fetchreporturl = 'https://sandbox.morpheusdata.com/api/reports/'+str(sid)
     response = requests.get(fetchreporturl, headers=headers)
     data = response.json()
-    print(data)
+    #This is for app Cost
+    #rows = data['reportResult']['rows'][2]
+    #This is for instance Cost
+    rows = data['reportResult']
+    print(rows)
 
-#getreports(sid)
+
+#time.sleep(20)
+getreports(418)
+#print(rdata)
 
 ##Things to do
-## Set the date range to 30 days
-## Format the report data in csv and send it via email
+## Format the report data return in rows to print Summary of Report and then 
+# loop the data to put them in colomns and write to CSV.
+
+##Things completed
+# The print rows in getreports now displays he content of the report what we see in UI
