62e3cf74efb69f39a3addbf31a5e77326eec36c3
Anish Abraham <vjanish1984@gmail.com>
2020-03-17 10:01:04 +0000

    Initial Commit

---
+++ Operational Workflow/operational_workflow.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+#token = "token goes here"
+#headers = {"Content-Type":"application/json","Accept":"application/json","Authorization": "Basic " + (token)}
+
+def executeJenkinsWorkflow():
+    #url = ""
+    body = {"businessService": "morpheus['instance']['hostname']"}
+    #"<%=customOptions.fvmbusinessservice%>", "location": "<%=customOptions.fvmlocation%>", "usage": "<%=customOptions.fvmusage%>","vmcpu": "<%=customOptions.fvmcpu%>","vmmemory": "<%=customOptions.fvmmemory%>","instancename": "rkalv<%=customOptions.fvminstancename%>","centrifyZone": "<%=customOptions.fvmcentrifyzone%>"}
+    b = json.dumps(body)
+    print(b)
+    #response = requests.post(generatereporturl, headers=headers, data=b)
+    #data = response.json()
+    #print(data)
+
+
+executeJenkinsWorkflow()
+    
