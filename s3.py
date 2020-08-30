1afd2d74d9a729d4f5eb6a1dc3ebb4e186aa7b0d
Anish Abraham <anish@cuxtud-2.local>
2020-08-30 12:52:46 +0100

    Added function to set tags

--- s3.py
+++ s3.py
@@ -7,4 +7,40 @@ def create_bucket(bucket_name):
     s3_client = boto3.client('s3')
     s3_client.create_bucket(Bucket=bucket_name)
 
+def set_object_keys(bucket_name, key, update=True, **new_tags):
+    """
+    Add/Update/Overwrite tags to AWS S3 Object
+
+    :param bucket_key: Name of the S3 Bucket
+    :param update: If True: appends new tags else overwrites all tags with **kwargs
+    :param new_tags: A dictionary of key:value pairs 
+    :return: True if successful 
+    """
+
+    #  I prefer to have this var outside of the method. Added for completeness
+    client = boto3.client('s3')   
+
+    old_tags = {}
+
+    if update:
+        old = client.get_object_tagging(
+            Bucket=bucket_name,
+            Key=key,
+        )
+
+        old_tags = {i['Key']: i['Value'] for i in old['TagSet']}
+
+    new_tags = {**old_tags, **new_tags}
+
+    response = client.put_object_tagging(
+        Bucket=bucket_name,
+        Key=key,
+        Tagging={
+            'TagSet': [{'Key': str(k), 'Value': str(v)} for k, v in new_tags.items()]
+        }
+    )
+
+    return response['ResponseMetadata']['HTTPStatusCode'] == 200
+
 create_bucket(mbname)
+set_object_keys(mbname, name="My Name", colour="purple")
