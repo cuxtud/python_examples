82a03df551ed985b379b2368eb825df0eb148dac
Anish Abraham <anish@cuxtud-2.local>
2020-08-30 10:30:52 +0100

    Update customOptions vars

--- s3.py
+++ s3.py
@@ -1,1 +1,8 @@
 import boto3
+
+mbname=morpheus['customOptions']['fbname']
+def create_bucket(bucket_name):
+    s3_client = boto3.client('s3')
+    s3_client.create_bucket(Bucket=bucket_name)
+
+create_bucket(mbname)
