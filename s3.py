a8b69131c8be80f99213131a0dd7d90448c35aca
Anish Abraham <anish@cuxtud-2.local>
2020-09-01 16:15:21 +0100

    Used fun for tags

--- s3.py
+++ s3.py
@@ -43,7 +43,9 @@ set_bucket_tags(mbname, key1="value1", key2="value2", key3="value3")
 def bucket_versioning(bucket_name):
     s3 = boto3.resource('s3')
     bucket_versioning = s3.BucketVersioning(bucket_name)
-    bucket_versioning.enable('Enabled')
+    response = bucket_versioning.enable(
+        MFA='string'
+    )
     
 
 bucket_versioning(mbname)
