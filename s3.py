46db4b81f7e51d97cbff4cccb2e7ee698736b9c1
Anish Abraham <anish@cuxtud-2.local>
2020-09-01 11:28:06 +0100

    Updated a few thingsas per the sheet

--- s3.py
+++ s3.py
@@ -6,7 +6,12 @@ mbname=morpheus['customOptions']['fbname']
 
 def create_bucket(bucket_name):
     s3_client = boto3.client('s3')
-    s3_client.create_bucket(Bucket=bucket_name)
+    s3_client.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={
+            'LocationConstraint': 'eu-west-1'
+        }
+    )
 
 #create bucket with the name provided when Operational workflow is executed
 create_bucket(mbname)
@@ -27,3 +32,35 @@ Set_tag = bucket_tagging.put(
         ]
     }
 )
+
+flogvalue=morpheus['customOptions']['flogvalue']
+
+#Setup Logging
+def blogging(logboolean):
+    if logboolean == 'yes':
+        s3 = boto3.resource('s3')
+        bucket_logging = s3.BucketLogging(mbname)
+        response = bucket_logging.put(
+            BucketLoggingStatus={
+                'LoggingEnabled': {
+                    'TargetBucket': mbname,
+                    'TargetGrants': [
+                        {
+                            'Grantee': {
+                                'DisplayName': 'username',
+                                'EmailAddress': 'emailid@mail.com',
+                                'ID': 'idvalue',
+                                'Type': 'CanonicalUser'|'AmazonCustomerByEmail'|'Group',
+                                'URI': 'string'
+                            },
+                            'Permission': 'FULL_CONTROL'
+                        },
+                    ],
+                    'TargetPrefix': 'string'
+                }
+            },
+        )
+    else:
+        print('Logging not requested')
+
+blogging(flogvalue)
