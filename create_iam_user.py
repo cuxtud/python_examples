34af336d126d7854dedb9aaf5baaf50e25e82af0
Anish Abraham <anish@cuxtud-2.local>
2020-09-11 15:09:15 +0100

    the last run was success. Adding some comments to explain the function

--- create_iam_user.py
+++ create_iam_user.py
@@ -6,7 +6,6 @@ def create_iam_User(usernameaa):
     iam = boto3.resource('iam')
     user = iam.User(usernameaa)
     user = user.create(
-    #Path='string',
     PermissionsBoundary='arn:aws:iam::370459551696:policy/Morpheus-S3',
     UserName = usernameaa,
     Tags=[
@@ -17,12 +16,16 @@ def create_iam_User(usernameaa):
     ]
 )
 
+#Add user to the group
+
 def addto_group(usernameaa):
     iam = boto3.resource('iam')
     user = iam.User(usernameaa)
     user = user.add_group(
     GroupName='Morpheus'
 )
+
+#create access and secret keys for the user
 def create_keys(usernameaa):
     iam = boto3.client('iam')
     response = iam.create_access_key(UserName=usernameaa)
