96f8efa824e666ee68a9744c06c37f5ee03e408c
Anish Abraham <anish@cuxtud-2.local>
2020-09-29 16:35:23 +0100

    Added new stuff

--- cypher.py
+++ cypher.py
@@ -1,5 +1,17 @@
 import morpheus-cypher
 from morpheuscypher import Cypher
+import os
+import sys
+from pprint import pprint
+print(sys.executable)
+print("morpheus['morpheus']:")
+pprint(morpheus['morpheus'])
+print("OS Env:")
+pprint(os.environ)
+print("sys.argv:")
+pprint(sys.argv)
+print("morpheus:")
+pprint(morpheus)
 c = Cypher(morpheus=morpheus)
 result = c.get("secret/testing:accounts:1")
 print(result)
