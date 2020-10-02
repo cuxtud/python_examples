84a80b304ba2ed69badb441812143d6886fdb576
Anish Abraham <anish@cuxtud-2.local>
2020-10-02 13:30:01 +0100

    Adding print for ip

--- morpheus/setupMorpheus.py
+++ morpheus/setupMorpheus.py
@@ -14,7 +14,7 @@ smorpheusurl="https://%s/api/setup/init" % (strip)
 nmorpheusurl=str(smorpheusurl)
 def setup():
     morpheusurl=nmorpheusurl
-    body={ "applianceName": "myenterprise-morpheus", "applianceUrl": aurl, "accountName": "Morpheus", "username": "admin", "password": "69f49632b13e", "email": "aabraham@morpheusdata.com", "firstName": firstname }
+    body={ "applianceName": "myenterprise-morpheus", "applianceUrl": aurl, "accountName": "Morpheus", "username": "admin", "password": "69f49!632b13e", "email": "aabraham@morpheusdata.com", "firstName": firstname }
     b = json.dumps(body)
     response = requests.post(morpheusurl, headers=headers, data=b, verify=False)
     data = response.json()
