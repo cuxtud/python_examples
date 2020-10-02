f1979984f4449ccfa109751108059894b9ea1c78
Anish Abraham <anish@cuxtud-2.local>
2020-10-02 16:38:04 +0100

    Testing

--- morpheus/setupMorpheus.py
+++ morpheus/setupMorpheus.py
@@ -25,20 +25,27 @@ time.sleep(600)
 setup()
 print("This Lab is for %s with email: %s. Login with username: admin and password: 69F49!632b13e") % (firstname,emailid)
 
-'''
 tokenurl=str("https://%s/oauth/token?grant_type=password&scope=write&client_id=morph-api"%(ip))
 tokenheader={'Content-Type': 'application/x-www-form-urlencoded'}
 #Get token of the appliance
 def token():
-    body = 'username=admin&password=69F49!632b13e'
-    response = requests.post(tokenurl, headers=tokenheader, body)
+    body = {'username': 'admin', 'password': '69F49!632b13e'}
+    response = requests.post(tokenurl, headers=tokenheader, data=body, verify=False)
+    data = response.json()
+    access_token = data['access_token']
+    return access_token
+
+
+access_token=token()
+print(access_token)
 
 #Add license to the appliance
-license_headers={'Content-Type': 'application/json,"Accept":"application/json","Authorization": "BEARER " + (token)'}
+license_headers={'Content-Type': 'application/json,"Accept":"application/json","Authorization": "BEARER " + (access_token)'}
 def license():
     body={"license": ""}
     b = json.dumps(body)
     response = requests.post(licenseurl, headers=license_headers, data=b, verify=False)
     data = response.json()
     print(data)
-'''
+
+license()
