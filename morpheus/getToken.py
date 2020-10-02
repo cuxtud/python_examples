89209b9fd7f53bc7c9ad39cd8cc3f71bd43dcf71
Anish Abraham <anish@cuxtud-2.local>
2020-10-02 14:53:32 +0100

    Updated getToken.py for ip of the applianceUrl

--- morpheus/getToken.py
+++ morpheus/getToken.py
@@ -6,6 +6,8 @@ tokenheader={'Content-Type': 'application/x-www-form-urlencoded'}
 #Get token of the appliance
 def token():
     body = 'username=admin&password=69F49!632b13e'
-    response = requests.post(tokenurl, headers=tokenheader, body)
+    response = requests.post(tokenurl, headers=tokenheader, body, verify=False)
     return response
     print(response)
+
+token()
