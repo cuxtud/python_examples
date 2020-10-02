638e3cc7544efe8df9dcc454e3c0847fdd7838f3
Anish Abraham <anish@cuxtud-2.local>
2020-10-02 13:48:40 +0100

    Updating password

--- morpheus/setupMorpheus.py
+++ morpheus/setupMorpheus.py
@@ -9,7 +9,7 @@ firstname=str(morpheus['customOptions']['ffirstname'])
 morpheusurl=str("https://%s/api/setup/init" % (ip))
 
 def setup():
-    body={ "applianceName": "myenterprise-morpheus", "applianceUrl": url, "accountName": "Morpheus", "username": "admin", "password": "69f49!632b13e", "email": emailid, "firstName": firstname }
+    body={ "applianceName": "myenterprise-morpheus", "applianceUrl": url, "accountName": "Morpheus", "username": "admin", "password": "69F49!632b13e", "email": emailid, "firstName": firstname }
     b = json.dumps(body)
     response = requests.post(morpheusurl, headers=headers, data=b, verify=False)
     data = response.json()
