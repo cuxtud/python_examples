ffd1c6695d537f22d37b8350caf80dc8355972e5
Anish Abraham <anish@cuxtud-2.local>
2020-10-02 14:48:05 +0100

    Updated getToken.py for ip of the applianceUrl

--- morpheus/getToken.py
+++ morpheus/getToken.py
@@ -1,8 +1,11 @@
 import request
 
+ip=str(morpheus['container']['externalIp'])
 tokenurl=str("https://%s/oauth/token?grant_type=password&scope=write&client_id=morph-api"%(ip))
 tokenheader={'Content-Type': 'application/x-www-form-urlencoded'}
 #Get token of the appliance
 def token():
     body = 'username=admin&password=69F49!632b13e'
     response = requests.post(tokenurl, headers=tokenheader, body)
+    return response
+    print(response)
