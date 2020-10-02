b58b58253251340235b6abe0923a97e850696fda
Anish Abraham <anish@cuxtud-2.local>
2020-10-02 13:18:25 +0100

    Adding print for ip

--- morpheus/setupMorpheus.py
+++ morpheus/setupMorpheus.py
@@ -9,8 +9,8 @@ url="https://%s" % (strip)
 nemailid=morpheus['customOptions']['fmoremailid']
 emailid=str(nemailid)
 firstname=morpheus['customOptions']['ffirstname']
-nmorpheusurl="https://%s/api/setup/init" % (strip)
-
+smorpheusurl="https://%s/api/setup/init" % (strip)
+nmorpheusurl=str(nmorpheusurl)
 def setup():
     morpheusurl=nmorpheusurl
     body={ "applianceName": "myenterprise-morpheus", "applianceUrl": url, "accountName": "Morpheus", "username": "admin", "password": "69f49632b13e", "email": emailid, "firstName": firstname }
