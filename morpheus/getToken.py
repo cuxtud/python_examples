297d83f4c2f09136aeccf1770b5c14c50b94d3d4
Anish Abraham <anish@cuxtud-2.local>
2020-10-02 15:40:29 +0100

    Updated getToken.py for module name typo

--- morpheus/getToken.py
+++ morpheus/getToken.py
@@ -5,7 +5,7 @@ tokenurl=str("https://%s/oauth/token?grant_type=password&scope=write&client_id=m
 tokenheader={'Content-Type': 'application/x-www-form-urlencoded'}
 #Get token of the appliance
 def token():
-    body = 'username=admin&password=69F49!632b13e'
+    body = {'username': 'admin', 'password': '69F49!632b13e'}
     response = requests.post(tokenurl, headers=tokenheader, data=body, verify=False)
     data = response.json()
     return data
