63d9922ba3124906f6455c26171321f1985dfb61
Anish Abraham <anish@cuxtud-2.local>
2020-10-04 10:24:05 +0100

    Testing to retrieve lab key

--- cypher.py
+++ cypher.py
@@ -3,8 +3,8 @@ import os
 import sys
 from pprint import pprint
 c = Cypher(morpheus=morpheus)
-result = c.get("secret/labkey:license:0")
+result = c.get("secret/labkey:license")
 print(result)
 print(result.values())
 pv=result.values()
-print(pv)
+print(pv[0:100])
