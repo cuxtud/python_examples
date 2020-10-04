074f4d9cb3cd269c4d3a20a4bb936d98a2961b86
Anish Abraham <anish@cuxtud-2.local>
2020-10-04 15:49:38 +0100

    Updated typo

--- morpheus/setupMorpheus.py
+++ morpheus/setupMorpheus.py
@@ -25,8 +25,8 @@ def setup():
 #time.sleep(1200)
 
 #execute setup to run the initial setup of the appliance
-#setup()
-#print("This Lab is for %s with email: %s. Login with username: admin and password: 69F49!632b13e") % (firstname,emailid)
+setup()
+print("This Lab is for %s with email: %s. Login with username: admin and password: 69F49!632b13e") % (firstname,emailid)
 
 tokenurl=str("https://%s/oauth/token?grant_type=password&scope=write&client_id=morph-api"%(ip))
 tokenheader={'Content-Type': 'application/x-www-form-urlencoded'}
