6d97d3954cbb234abf0cf8000554882d6461bebe
Anish Abraham <anish@cuxtud-2.local>
2020-10-05 14:53:41 +0100

    Updated typo

--- morpheus/cliInstall.sh
+++ morpheus/cliInstall.sh
@@ -4,4 +4,4 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm install 2.5.1
-gem installl morpheus-cli
+gem install morpheus-cli
