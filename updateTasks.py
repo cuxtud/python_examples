9c3003aafa9463e87c022ff3ca89bce8587bdaa0
Anish Abraham <anish@cuxtud-2.local>
2020-10-08 15:52:52 +0100

    Testing a few things

--- updateTasks.py
+++ updateTasks.py
@@ -7,15 +7,18 @@ from pprint import pprint
 c = Cypher(morpheus=morpheus)
 access_token=morpheus['morpheus']['apiAccessToken']
 host=morpheus['morpheus']['applianceHost']
+print(host)
 
 #get all tasks of type ansible and parse the id and then update the execute target
 
 headers = {"Content-Type":"application/json","Accept":"application/json","Authorization": "BEARER " + (access_token)}
 apiUrl = str('https://%s/api/tasks?taskTypeCodes=ansibleTowerTask' % (host))
+print(apiUrl)
 
 response = requests.get(apiUrl, headers=headers, verify=False)
 data = response.json()
 tasks=data['tasks']
+print(tasks)
 
 for i in tasks:
     for k,v in i.items():
