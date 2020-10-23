0da5a1886b573d10eaeb7fcbd8b46d8b40812566
Anish Abraham <anish@cuxtud-2.local>
2020-10-23 22:20:38 +0100

    Adding py script to create CI relationship

--- create_cmdb_ci_relation.py
+++ create_cmdb_ci_relation.py
@@ -18,36 +18,28 @@ if response.status_code != 200:
 # Decode the JSON response into a dictionary and use the data
 data = response.json()
 result = data['result']
-jresult= json.loads(result)
-sys_id=jresult['sys_id']
-print(sys_id)
+jresult= json.dumps(result)
 
-
-'''
-def create_relation(sys_id):
-    url = 'https://dev47749.service-now.com/api/now/cmdb/instance/cmdb_ci_linux_server/64ab8da09f030200fe2ab0aec32e701b/relation?sysparm_fields=sys_id%2Cname'
-    # Eg. User name="admin", Password="admin" for this code sample.
-    user = 'admin'
-    pwd = 'admin'
-    # Set proper headers
-    headers = {"Content-Type":"application/json","Accept":"application/json"}
-    # Do the HTTP request
-
-    response = requests.post(url, auth=(user, pwd), headers=headers ,data="{
-        \"inbound_relations\": [
-            {
-                \"type\": \"5f985e0ec0a8010e00a9714f2a172815\",
-                \"target\": \"45c60ced2f046c1026e0d6c6f699b620\",
-                \"sys_class_name\": \"cmdb_ci_apache_webserver\"
-            }
-        ],
-        \"source\": \"ServiceNow\"
-    }")
-    # Check for HTTP codes other than 200
-    if response.status_code != 200: 
-        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
-        exit()
-    # Decode the JSON response into a dictionary and use the data
-    data = response.json()
-    print(data)
-'''
+for i in result:
+    for k,v in i.items():
+        if k == 'sys_id':
+            jbody={"inbound_relations": [{"type": "5f985e0ec0a8010e00a9714f2a172815", "target": "5f8af237c0a8010e01a932999468b83a", "sys_class_name": "cmdb_ci_apache_web_server"}], "source": "ServiceNow" }
+            body=json.dumps(jbody)
+            print(v)
+            value=str(v)
+            print(value)
+            url = 'https://ven01434.service-now.com/api/now/cmdb/instance/cmdb_ci_vm_instance/%s/relation' % (value)
+            # Eg. User name="admin", Password="admin" for this code sample.
+            user = 'admin'
+            pwd = 'Pa55w0rd'
+            # Set proper headers
+            headers = {"Content-Type":"application/json","Accept":"application/json"}
+            # Do the HTTP request
+            response = requests.get(url, auth=(user, pwd), headers=headers )
+            # Check for HTTP codes other than 200
+            if response.status_code != 200: 
+                print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+                exit()
+            # Decode the JSON response into a dictionary and use the data
+            data = response.json()
+            print(data)
