7870bf9f82b5d69c0c5e0bf265e8c93d28155721
Anish Abraham <anish@cuxtud-2.local>
2020-10-23 21:26:43 +0100

    Adding py script to create CI relationship

--- create_cmdb_ci_relation.py
+++ create_cmdb_ci_relation.py
@@ -3,7 +3,7 @@
 import requests
 # Set the request parameters
 instance_name=morpheus['instance']['name']
-url = 'https://dev47749.service-now.com/api/now/cmdb/instance/cmdb_ci_server?sysparm_query=nameCONTAINS%s&sysparm_limit=1' (instance_name)
+url = 'https://dev47749.service-now.com/api/now/cmdb/instance/cmdb_ci_server?sysparm_query=nameCONTAINS%s&sysparm_limit=1' % (instance_name)
 # Eg. User name="admin", Password="admin" for this code sample.
 user = 'admin'
 pwd = 'Morpheus1!'
@@ -17,4 +17,34 @@ if response.status_code != 200:
     exit()
 # Decode the JSON response into a dictionary and use the data
 data = response.json()
-print(data)
+sys_id = data['results']
+print(sys_id)
+
+
+
+def create_relation(sys_id):
+    url = 'https://dev47749.service-now.com/api/now/cmdb/instance/cmdb_ci_linux_server/64ab8da09f030200fe2ab0aec32e701b/relation?sysparm_fields=sys_id%2Cname'
+    # Eg. User name="admin", Password="admin" for this code sample.
+    user = 'admin'
+    pwd = 'admin'
+    # Set proper headers
+    headers = {"Content-Type":"application/json","Accept":"application/json"}
+    # Do the HTTP request
+
+    response = requests.post(url, auth=(user, pwd), headers=headers ,data="{
+        \"inbound_relations\": [
+            {
+                \"type\": \"5f985e0ec0a8010e00a9714f2a172815\",
+                \"target\": \"45c60ced2f046c1026e0d6c6f699b620\",
+                \"sys_class_name\": \"cmdb_ci_apache_webserver\"
+            }
+        ],
+        \"source\": \"ServiceNow\"
+    }")
+    # Check for HTTP codes other than 200
+    if response.status_code != 200: 
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+        exit()
+    # Decode the JSON response into a dictionary and use the data
+    data = response.json()
+    print(data)
