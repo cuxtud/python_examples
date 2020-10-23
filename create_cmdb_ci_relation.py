fa4abb2f6b96c2f534759b74585b74a0a684081f
Anish Abraham <anish@cuxtud-2.local>
2020-10-23 22:23:46 +0100

    Adding py script to create CI relationship

--- create_cmdb_ci_relation.py
+++ create_cmdb_ci_relation.py
@@ -35,7 +35,7 @@ for i in result:
             # Set proper headers
             headers = {"Content-Type":"application/json","Accept":"application/json"}
             # Do the HTTP request
-            response = requests.get(url, auth=(user, pwd), headers=headers )
+            response = requests.post(url, auth=(user, pwd), headers=headers, data=body )
             # Check for HTTP codes other than 200
             if response.status_code != 200: 
                 print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
