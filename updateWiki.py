db37a04083dbd4dbf10b6480835656e58726f0d3
Anish Abraham <anish@cuxtud-2.local>
2020-11-10 17:31:19 +0000

    Initial update

--- updateWiki.py
+++ updateWiki.py
@@ -1,4 +1,20 @@
 import requests
+#Author: Anish Abraham
+#This script will fetch the users apiToken, appliance host and update some content on the instance wiki page
+#collecting data about appliance
 
-token=morpheus['morpheus']['morpheus']['apiAccessToken']
-print(token)
+host=morpheus['morpheus']['applianceHost']
+token=morpheus['morpheus']['apiAccessToken']
+instanceid=morpheus['instance']['id']
+user=morpheus['instance']['createdByUsername']
+jbody={"page": {"content": "**Instance Details**\r\n\r\nProvisioned by: %s \r\nData of provision: 23/10/2020\r\nProject: someproject\r\nOwner: Jane Doe\r\n\r\n**Support hours**\r\n8am to 8pm GMT\r\n\r\n**Support days**\r\nMonday to Sunday" % user} } 
+body=json.dumps(jbody)
+headers = {"Content-Type":"application/json","Accept":"application/json","Authorization": "BEARER " + (token)}
+apiUrl = 'https://%s/api/instances/%d/wiki' % (host, instanceid)
+url=str(apiUrl)
+
+#API request to update the Wiki page
+r = requests.put(url, headers=headers, data=body, verify=False)
+
+#Returns success code
+print(r)
