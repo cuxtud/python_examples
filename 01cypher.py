efc93dc33e8cb129c62f861ec077135da2970a48
Anish Abraham <anish@cuxtud-2.local>
2020-11-16 12:59:02 +0000

    Update cypher to test which value it returns

--- 01cypher.py
+++ 01cypher.py
@@ -3,7 +3,7 @@ import os
 import sys
 from pprint import pprint
 c = Cypher(morpheus=morpheus)
-result = c.get("secret/testing:accounts:1")
+result = c.get("secret/testing:accounts:1:1")
 pv=result.values()
 print(pv)
 #kv=(str(pv).strip('[]'))[2:-1]
