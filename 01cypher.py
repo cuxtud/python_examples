2b9e81e97b79d7cb92c23441b295b49c3b7ae06d
Anish Abraham <anish@cuxtud-2.local>
2020-12-14 13:36:30 +0000

    Updating...

--- 01cypher.py
+++ 01cypher.py
@@ -3,7 +3,7 @@ import os
 import sys
 from pprint import pprint
 c = Cypher(morpheus=morpheus)
-result = c.get("secret/testing:accounts:1[0]")
+result = c.get("secret/testing:accounts:1")
 pv=result.values()
 #kv=(str(pv).strip('[]'))[2:-1]
 #print(kv)
